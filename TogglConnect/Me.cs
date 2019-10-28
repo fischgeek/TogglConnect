a73703ce97e239df2bba1b17d9e5b06a925b8eb3
Jeremiah Fischer <fischgeek@gmail.com>
2019-10-28 14:19:24 -0500

    quick

---
+++ TogglConnect/Me.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogglConnect
+{
+    public class UserData
+    {
+        public int id { get; set; }
+        public string api_token { get; set; }
+        public int default_wid { get; set; }
+        public string email { get; set; }
+        public string fullname { get; set; }
+        public string jquery_timeofday_format { get; set; }
+        public string jquery_date_format { get; set; }
+        public string timeofday_format { get; set; }
+        public string date_format { get; set; }
+        public bool store_start_and_stop_time { get; set; }
+        public int beginning_of_week { get; set; }
+        public string language { get; set; }
+        public string duration_format { get; set; }
+        public string image_url { get; set; }
+        public DateTime at { get; set; }
+        public DateTime created_at { get; set; }
+        public string timezone { get; set; }
+        public int retention { get; set; }
+        public List<Project> projects { get; set; }
+        //public List<Tag> tags { get; set; }
+        //public List<Task> tasks { get; set; }
+        //public List<Workspace> workspaces { get; set; }
+        public List<Client> clients { get; set; }
+    }
+    public class User
+    {
+        public int since { get; set; }
+        public UserData data { get; set; }
+    }
+}
--- TogglConnect/TogglBase.cs
+++ TogglConnect/TogglBase.cs
@@ -21,7 +21,15 @@ namespace TogglConnect
         }
 
         private static string baseUrl = "https://www.toggl.com/api/v8";
+        public string username { get; set; }
+        public string password { get; set; }
         public string apiKey { get; set; }
+        public string authString
+        {
+            get {
+                return JFUtil.Base64Encode($"{this.apiKey}:api_token");
+            }
+        }
         public int wid { get; set; }
 
         public void Init(string apiKey, int wid)
@@ -30,23 +38,40 @@ namespace TogglConnect
             this.wid = wid;
         }
 
+        public void Init(string apiKey, string username, string password, int wid)
+        {
+            this.apiKey = apiKey;
+            this.username = username;
+            this.password = password;
+            this.wid = wid;
+        }
+
+        public User GetMe()
+        {
+            var url = $"{baseUrl}/me";
+            var x = WebCall.GetRequestWithErrorHandling(url, this.authString);
+            var t = JsonConvert.DeserializeObject<User>(x.Data);
+            return t;
+        }
+
         public void GetClients()
         {
             var url = $"{baseUrl}/clients";
-            var x = WebCall.GetRequestWithErrorHandling(url, this.apiKey);
+            var x = WebCall.GetRequestWithErrorHandling(url, this.authString);
             var t = JsonConvert.DeserializeObject<List<Client>>(x.Data);
         }
 
-        public void GetProjects()
+        public List<Project> GetProjects()
         {
             var url = $"{baseUrl}/workspaces/{this.wid}/projects";
-            var x = WebCall.GetRequestWithErrorHandling(url, this.apiKey);
+            var x = WebCall.GetRequestWithErrorHandling(url, this.authString);
             var t = JsonConvert.DeserializeObject<List<Project>>(x.Data);
+            return t;
         }
 
         public TimeEntry GetRunningTimer()
         {
-            var x = WebCall.GetRequestWithErrorHandling($"{baseUrl}/time_entries/current", this.apiKey);
+            var x = WebCall.GetRequestWithErrorHandling($"{baseUrl}/time_entries/current", this.authString);
             var t = JsonConvert.DeserializeObject<TimeEntryData>(x.Data);
             var timeEntry = t.data;
             return timeEntry;
@@ -55,7 +80,7 @@ namespace TogglConnect
         public void StartTimer(TimeEntryWrapper wrapper)
         {
             var url = $"{baseUrl}/time_entries/start";
-            var x = WebCall.PostRequestWithErrorHandling(url, wrapper, this.apiKey);
+            var x = WebCall.PostRequestWithErrorHandling(url, wrapper, this.authString);
         }
 
         public void StopRunningTimer()
@@ -63,7 +88,7 @@ namespace TogglConnect
             var te = GetRunningTimer();
             if (te != null) {
                 var url = $"{baseUrl}/time_entries/{te.id}/stop";
-                WebCall.PutRequest(url, basicAuth: this.apiKey);
+                WebCall.PutRequest(url, basicAuth: this.authString);
             }
         }
 
